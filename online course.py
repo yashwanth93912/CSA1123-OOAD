00ca1c7bb9b0b7445d975986585a74019da13a2b
yashwanth93912 <130470543+yashwanth93912@users.noreply.github.com>
2023-11-30 10:59:28 +0530

    Add files via upload

---
+++ online course.py
@@ -0,0 +1,41 @@
+# coding=System
+from entrollment import *
+from arms_site import *
+from new_class import *
+
+class login(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  username  (private)
+
+   
+
+  passwodr  (private)
+
+   
+
+  email_  (private)
+
+  """
+
+  def login(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
---
+++ trade.py
@@ -0,0 +1,48 @@
+# coding=System
+from biling import *
+
+class sell_stock(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  details  (private)
+
+   
+
+  bulk  (private)
+
+   
+
+  type  (private)
+
+  """
+
+  def check(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def view(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
